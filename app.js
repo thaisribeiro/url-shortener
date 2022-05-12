d879f411a95537f9b7cba8aecb97b3e3288a6aa8
Thais Ribeiro <thaisribeiro@softbox.com.br>
2022-05-12 11:37:56 -0300

    port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const urlShortener = require("node-url-shortener");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
