2716b0a219bf6fae60a37271878f861e0ffbcfeb
Thais Ribeiro <thaisribeiro@softbox.com.br>
2022-05-12 11:13:51 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const urlShortener = require("node-url-shortener");
+
+const port = 3000;
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded());
+
+app.post("/url", function (req, res) {
+  const url = req.body.url;
+  urlShortener.short(url, function (err, shortUrl) {
+    res.send(shortUrl);
+  });
+});
+
+app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
